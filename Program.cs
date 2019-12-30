9bc31195528ebc71fbd48fa0c8e2d18b550f6199
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-30 11:47:22 +0100

    App skeleton

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "this is the first line", "this is the second line", "This is thethird line" };
+            File.WriteAllLines("MyFirstFile.txt", lines);
+
+            string[] fileContent = File.ReadAllLines("MyFirstFile.txt");
+
+            foreach(string line in fileContent)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
